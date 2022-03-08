5f665427affca16eedfd5613f37617a51d87acd4
Yannick <91213045+YannickZahinda@users.noreply.github.com>
2022-03-08 16:04:09 +0200

    Fixed background, stick to figma design

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-const menuList = document.getElementById("menu-list");
-const hambergerMenu = document.getElementById("menu-icon");
+const menuList = document.getElementById('menu-list');
+const hambergerMenu = document.getElementById('menu-icon');
 const closingIcon = document.getElementById('menu-closing-icon');
-const navLink = document.getElementById("menu-list");
 
-hambergerMenu.addEventListener('click', (e)=>{
-    menuList.classList.toggle("active");
+hambergerMenu.addEventListener('click', ()=>{
+    menuList.classList.toggle('active');
     closingIcon.style.display = "block";
 })
 
-closingIcon.addEventListener("click", (e) => {
+closingIcon.addEventListener("click", () => {
     menuList.classList.remove("active");
 })
 
-document.querySelectorAll(".menu-link").forEach(n => n.addEventListener("click", () => {
+document.querySelectorAll('.menu-link').forEach(n => n.addEventListener("click", () => {
     menuList.classList.remove("active");
     hambergerMenu.classList.remove('active');
 }))
